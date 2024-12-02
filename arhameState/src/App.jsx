9a3328b892c60e309b714603497b27cdbba794a9
Kashif Ullah <kashifullah.dev@gmail.com>
2024-12-02 22:45:29 +0500

    added code

---
+++ arhameState/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SigUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
+        <Route path='/sign-up' element={<SigUp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
