3b10cd903ccc699654352ea05c8c11ec856f69fc
Kashif Ullah <kashifullah.dev@gmail.com>
2024-12-07 18:08:35 +0500

    added redux persist

--- arhameState/src/store/store.js
+++ arhameState/src/store/store.js
@@ -1,17 +1,32 @@
 // store/store.js
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/baseApi";
 import { userApi } from "./api/authSlice";
 import userSlice from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 userSlice;
+
+const rootReducer = combineReducers({ userReducer: userSlice });
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer, // Add RTK Query reducer
     [userApi.reducerPath]: userApi.reducer,
-    userReducer: userSlice,
+    persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(baseApi.middleware), // Add RTK Query middleware
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }).concat(baseApi.middleware), // Add RTK Query middleware
 });
 
 export default store;
+export const persistor = persistStore(store);
